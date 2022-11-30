cea7ac95711b461512cc94098c307cfbba105974
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-11-30 23:03:37 +0900

    update: add Doller struct and times function

--- chap_01_money/src/lib.rs
+++ chap_01_money/src/lib.rs
@@ -1,8 +1,27 @@
+struct Doller {
+    amount: i32,
+}
+
+impl Doller {
+    fn new(amount: i32) -> Doller {
+        Doller {
+            amount,
+        }
+    }
+
+    fn times(&self, multiplier: i32) -> Self{
+        Doller::new(self.amount * multiplier)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn multiplication() {
+        let five = Doller::new(5);
+        let ten = five.times(2);
+        assert_eq!(10, ten.amount);
     }
 }
