78d97f716753584f64efb6820bc393636d62701e
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-12-02 09:45:14 +0900

    impl: Doller equals / p24

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,10 @@ impl Doller {
             amount: self.amount * multiplier,
         }
     }
+
+    fn equals(&self, doller: &Doller) -> bool {
+        self.amount == doller.amount
+    }
 }
 
 #[cfg(test)]
@@ -25,5 +29,11 @@ mod tests {
         assert_eq!(10, product.amount);
         let product = five.times(3);
         assert_eq!(15, product.amount);
+    }
+
+    #[test]
+    fn equality() {
+        assert!(Doller::new(5).equals(&Doller::new(5)));
+        assert!(!Doller::new(5).equals(&Doller::new(6)));
     }
 }
